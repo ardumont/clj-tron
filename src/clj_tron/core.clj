32a03be6c7160d5c814a7038d2ea391a1b7b5d82
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-09-06 17:09:16 +0200

    Refactor the strategy policy writings.

--- src/clj_tron/core.clj
+++ src/clj_tron/core.clj
@@ -3,7 +3,7 @@
              [pprint :as p]
              [string :as s]]))
 
-(def arena
+#_(def arena
   (vec (repeat 10 (repeat 10 nil))))
 
 (defn make-arena
@@ -31,32 +31,65 @@
        (ref-set (get-in arena [(dec w) j]) :wall)))
     arena))
 
-(def arena (make-arena 10 10))
+#_(def arena (make-arena 10 10))
 
-(p/pprint arena)
+#_(p/pprint arena)
 
 (defn print-arena
   [arena]
   (dosync
    (doseq [row arena]
      (let [vals (map deref row)
-           chars (map {:wall \X nil \space} vals)]
+           chars (map #({:wall \X nil \space} % %) vals)]
        (println (apply str chars))))))
+
+#_(print-arena arena)
+
+(defn stubborn-bot-factory
+  "Stubborn bot that goes straight ahead"
+  [[di dj]]
+  (fn [[i j]]
+    [(+ i di) (+ j dj)]))
+
+(defn stubborn-bot-factory
+  "Drunk bot that goes randomly"
+  [[di dj]]
+  {:strategy (fn [[i j] _]
+               {:pos [(+ i di) (+ j dj)]
+                :state nil})
+   :state nil})
+
+(defn avoid-bot-factory
+  "Generates bot that goes straight ahead and avoid obstacles!"
+  )
+
+(defn play
+  "Play the strategy for the bot bot. A strategy takes one position or nil if impossible."
+  [name {bot :strategy
+         init-state :state} init-pos]
+  (loop [{pos :pos
+          bot-state :state}
+         {:pos init-pos
+          :state init-state}]
+    (when pos
+      (Thread/sleep 2000)
+      (recur
+       (dosync
+        (let [r (get-in arena pos)]
+          (if @r
+            (println name "ARGGGGGHHHHH! BOUM!")
+            (do
+              (ref-set r name)
+              (bot pos bot-state)))))))))
+
+#_(play 1 (stubborn-bot-factory [1 0]) [3 3])
+
+(def arena (make-arena 20 20))
 
 (print-arena arena)
 
-(defn stubborn-bot
-  "Stubborn bot that goes straight ahead"
-  [name [i j :as pos] [di dj]]
-  (dosync
-   (let [r (get-in arena pos)]
-     (if @r
-       (println name "-> boum!")
-       (do
-         (ref-set r name)
-         [(+ i di) (+ j dj)])))))
+(do
+  (future (play \o (stubborn-bot-factory [1 0])  [1 1]))
+  (future (play \z (stubborn-bot-factory [0 -1]) [8 8])))
 
-(defn play
-  "Play the strategy for the bot bot"
-  [bot]
-  )
+
