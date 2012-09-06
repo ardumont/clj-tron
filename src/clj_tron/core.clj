cc542525013be089f2acdc71d803b5ddcc12f8ae
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-09-06 15:26:39 +0200

    Init an arena

--- src/clj_tron/core.clj
+++ src/clj_tron/core.clj
@@ -1,6 +1,14 @@
-(ns clj-tron.core)
+(ns clj-tron.core
+  (:require [clojure.pprint :as p]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def arena
+  (vec (repeat 10 (repeat 10 nil))))
+
+(defn make-arena
+  "Build a new arena nxn"
+  [n]
+  (vec (repeat n (repeat n nil))))
+
+(def arena (make-arena 10))
+
+(p/pprint arena)
