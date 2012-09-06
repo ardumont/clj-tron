2e5c1a0eca8dea903b5d1f65de5f45807b4f9a68
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-09-06 15:56:29 +0200

    Print the arena (no moto yet)

--- src/clj_tron/core.clj
+++ src/clj_tron/core.clj
@@ -1,5 +1,7 @@
 (ns clj-tron.core
-  (:require [clojure.pprint :as p]))
+  (:require [clojure
+             [pprint :as p]
+             [string :as s]]))
 
 (def arena
   (vec (repeat 10 (repeat 10 nil))))
@@ -29,7 +31,14 @@
        (ref-set (get-in arena [(dec w) j]) :wall)))
     arena))
 
-(def arena (make-arena 10 10))
+(def arena (make-arena 10 10)q)
 
 (p/pprint arena)
+
+(defn print-arena
+  [arena]
+  (doseq [row arena]
+    (let [vals (map deref row)
+          chars (map {:wall \X nil \space} vals)]
+      (println (apply str chars)))))
 
