fac7d51dca03e521329a1136cbce3296b0f485b3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-09-08 14:14:46 +0200

    Refactor the computing of the next direction and position.

--- src/clj_tron/bots.clj
+++ src/clj_tron/bots.clj
@@ -8,22 +8,48 @@
                 :state nil})
    :state nil})
 
-;; Behaviour to turn left at each obstacle
-(def to-left
+;; Behaviour to turn right at each obstacle
+(def to-right
   {[0 1]  [-1 0]
    [-1 0] [0 -1]
    [0 -1] [1 0]
    [1 0]  [0 1]})
 
-(defn avoider-bot-factory
-  "Generates bot that goes straight ahead and avoid obstacles!"
+(defn- new-pos-dir
+  "Compute the new position and the new direction and return a vector [new-pos new-dir]"
+  [pos dir new-dir-fn]
+  (let [new-dir (new-dir-fn dir)]
+    {:pos (map + pos new-dir)
+     :state new-dir}))
+
+(defn avoider-right-bot-factory
+  "Generates bot that goes straight ahead and avoid obstacles by turning right!"
   [arena [di dj :as dir]]
   {:strategy
    (fn [pos dir]
      (let [new-pos (map + pos dir)]
        (if @(get-in arena new-pos)
-         (let [new-dir (to-left dir)
-               new-pos (map + pos new-dir)]
-           {:pos new-pos :state new-dir})
+         (new-pos-dir pos dir to-right)
+         {:pos new-pos :state dir})))
+   :state dir})
+
+;; Behaviour to turn left at each obstacle
+(def to-left
+  {[0 1]  [1 0]
+   [-1 0] [0 1]
+   [0 -1] [-1 0]
+   [1 0]  [0 -1]})
+
+(defn avoider-bot-factory
+  "Generates bot that goes straight ahead and avoid obstacles by trying to turn left or right!"
+  [arena [di dj :as dir]]
+  {:strategy
+   (fn [pos dir]
+     (let [new-pos (map + pos dir)]
+       (if @(get-in arena new-pos)
+         (let [{:keys [new-dir-r new-pos-r] :as right} (new-pos-dir pos dir to-right)]
+           (if @(get-in arena new-pos-r)
+             (new-pos-dir pos dir to-left))
+           right)
          {:pos new-pos :state dir})))
    :state dir})
